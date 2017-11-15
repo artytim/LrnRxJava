5fca3ad7b47f355802440642e1c06785c8a6c78c
artytim <arty.tim@gmail.com>
2017-11-15 10:12:52 +0800

    Add libraries

--- src/main/java/Ch3_BasicOperators/L1_Suppressing_filter.java
+++ src/main/java/Ch3_BasicOperators/L1_Suppressing_filter.java
@@ -2,7 +2,7 @@ package Ch3_BasicOperators;
 
 import io.reactivex.Observable;
 
-public class Ch3_1 {
+public class L1_Suppressing_filter {
     public static void main(String[] args) {
         Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
                 .filter(s -> s.length() != 5)
